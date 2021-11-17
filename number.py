ce5c2b6f060ae220335877fea466217ebf380670
adnanibrahi0102 <91428394+adnanibrahi0102@users.noreply.github.com>
2021-11-17 12:10:07 +0530

    Add files via upload
    printing positive numbers in given range

---
+++ number.py
@@ -0,0 +1,7 @@
+# Python program to print positive Numbers in given range
+list1=[12,-3,45,-2,-9,4,5,7,8,99,121,-44,-55,98]
+#iterating each number in list
+for num in list1:
+    #checking conditions
+    if num >=0:
+        print(num,end=" ")
